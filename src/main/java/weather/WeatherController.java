c7fc62b685f776fb860e6866563d295fb3650915
Triinu Tasa <triinu.tasa@gmail.com>
2018-10-05 14:01:29 +0300

    Adding tests

--- src/main/java/weather/WeatherController.java
+++ src/main/java/weather/WeatherController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class IlmaController {
+public class WeatherController {
 
     @RequestMapping("/")
     public String index() {
